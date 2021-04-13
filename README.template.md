4d39be8881a76b71714e48ec57af1c016f88d811
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-13 20:21:16 +0200

    Initial commit

---
+++ README.template.md
@@ -0,0 +1,23 @@
+# My Title
+> My subtitle
+
+
+## Set up and run
+
+### Remote use
+
+1. Go to the repo on **GitHub**.
+2. Go to the _Settings_ section.
+3. Enable **GitHub Pages** for the _docs_ directory.
+4. Click _environment_ tab on your repo and for the site to build.
+5. On success, click _View deployment_ to see the public URL.
+6. Add the uRL to your Github repo's URL field and your README, to make it easy for people to find.
+
+### Serve locally
+
+To view the docs site on a local web server, follow these instructions - [Serve a docs site locally with DocsifyJS](https://gist.github.com/MichaelCurrin/4c8060dcc9d8841f842eeebc7a1436d8)
+
+
+## Resources
+
+For more info on what DocsifyJS is and how to use it, see [DocsifyJS Tutorial](https://michaelcurrin.github.io/docsify-js-tutorial/#/?id=serve-a-docsify-site-locally).
---
+++ replace.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Replacement utility script.
+#
+# Follow instructions in README.md to use this script and then delete it when you're done.
+
+set -e
+cd docs
+
+## MODIFY THESE WITH YOUR VALUES
+
+MY_TITLE='Foo Bar'
+MY_SUBTITLE='Foos the bar all the buzz'
+MY_USERNAME='My-Github-Username'
+MY_PROJECT_SLUG='foo-bar'
+
+## DO NOT MODIFY AFTER THE LINE ABOVE
+
+MY_REPO="$MY_USERNAME\/$MY_PROJECT_SLUG"
+MY_GETTING_STARTED="$MY_PROJECT_SLUG-template-homepage"
+
+ORIGINAL_TITLE='DocsifyJS Template'
+ORIGINAL_SUBTITLE='A minimal quickstart template for a docs site'
+ORIGINAL_REPO='MichaelCurrin\/docsify-js-template'
+ORIGINAL_GETTING_STARTED='docsifyjs-template-homepage'
+ORIGINAL_KEYWORDS='doc,docs,documentation,docsify,docsifyjs,github,jekyll,github-pages'
+
+PATTERN="s/$ORIGINAL_TITLE/$MY_TITLE/g
+s/$ORIGINAL_REPO/$MY_REPO/g
+s/$MY_GETTING_STARTED/$ORIGINAL_GETTING_STARTED/g
+s/$ORIGINAL_SUBTITLE/$MY_SUBTITLE/g
+s/$ORIGINAL_KEYWORDS/$MY_KEYWORDS/g"
+
+echo 'PATTERN'
+echo "$PATTERN"
+
+sed -i "$PATTERN" *
+
+echo "Done!"
